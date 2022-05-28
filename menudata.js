f110099382db22705b3dbbcfd24a40561e0b8efe
ArneCode <arne.deborman@web.de>
2022-05-28 13:41:26 +0000

    automatic commit

--- menudata.js
+++ menudata.js
@@ -26,7 +26,6 @@ var menudata={children:[
 {text:"Hauptseite",url:"index.html"},
 {text:"Klassen",url:"annotated.html",children:[
 {text:"Auflistung der Klassen",url:"annotated.html"},
-{text:"Klassen-Verzeichnis",url:"classes.html"},
 {text:"Klassenhierarchie",url:"inherits.html"},
 {text:"Klassen-Elemente",url:"functions.html",children:[
 {text:"Alle",url:"functions.html",children:[
