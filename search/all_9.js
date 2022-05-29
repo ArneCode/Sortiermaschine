a7d4e61be85d14efd28858dd1767c575717fae06
ArneCode <arne.deborman@web.de>
2022-05-29 12:52:50 +0000

    automatic commit

--- search/all_9.js
+++ search/all_9.js
@@ -7,7 +7,7 @@ var searchData=
   ['lcddotanim_4',['LcdDotAnim',['../classLcdDotAnim.html#aba9b5e75d3dbc072cf0203d5116e266c',1,'LcdDotAnim::LcdDotAnim()'],['../classLcdDotAnim.html',1,'LcdDotAnim']]],
   ['lcdloadinganim_5',['LcdLoadingAnim',['../classLcdLoadingAnim.html',1,'']]],
   ['lcdstring_6',['LcdString',['../structLcdString.html#a2afc90b22de35ebb93a337ec604856b8',1,'LcdString::LcdString()'],['../classAnimString.html#a2afc90b22de35ebb93a337ec604856b8',1,'AnimString::LcdString()'],['../structLcdString.html',1,'LcdString']]],
-  ['legballab_7',['legBallAb',['../sketch_8ino.html#a39b44b8ad8390b202d1c91f96e22d7a2',1,'sketch.ino']]],
+  ['legballab_7',['legBallAb',['../sketch_8ino.html#a6afc180dd30470854b71b1a9877a0d39',1,'sketch.ino']]],
   ['loading_5fbar_5foffset_8',['LOADING_BAR_OFFSET',['../animLcd_8h.html#a4fb812c2fb39bac66bd2170c07099870',1,'animLcd.h']]],
   ['loading_5fduration_9',['LOADING_DURATION',['../sketch_8ino.html#ab09894308b534e1030021de3bc84710e',1,'sketch.ino']]],
   ['loading_5fempty_5fc_10',['loading_empty_c',['../animLcd_8ino.html#af9de0017ae44e85326d55921c478325c',1,'animLcd.ino']]],
