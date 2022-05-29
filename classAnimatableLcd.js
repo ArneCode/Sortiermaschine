2d5be8acabeffc1bb864383135d6a5f7eb12490a
ArneCode <arne.deborman@web.de>
2022-05-29 12:48:23 +0000

    automatic commit

--- classAnimatableLcd.js
+++ classAnimatableLcd.js
@@ -1,6 +1,7 @@
 var classAnimatableLcd =
 [
     [ "init", "classAnimatableLcd.html#a0f93bbf8c49a76e17272721e67a24b38", null ],
+    [ "print", "classAnimatableLcd.html#aca170e34fe99df76f7a1e4f4241878c1", null ],
     [ "print", "classAnimatableLcd.html#ae9f318e41e16a8527eaeffa9500831ca", null ],
     [ "printCentered", "classAnimatableLcd.html#aaff767fb418dbc3e902dd4a1be3207a8", null ],
     [ "printPretty", "classAnimatableLcd.html#ad5509573d45cc84cda93b125fdca749e", null ],
--- search/functions_6.js
+++ search/functions_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['print_0',['print',['../classAnimatableLcd.html#ae9f318e41e16a8527eaeffa9500831ca',1,'AnimatableLcd']]],
+  ['print_0',['print',['../classAnimatableLcd.html#ae9f318e41e16a8527eaeffa9500831ca',1,'AnimatableLcd::print(const String &amp;text)'],['../classAnimatableLcd.html#aca170e34fe99df76f7a1e4f4241878c1',1,'AnimatableLcd::print(const String &amp;&amp;text)']]],
   ['printcentered_1',['printCentered',['../classAnimatableLcd.html#aaff767fb418dbc3e902dd4a1be3207a8',1,'AnimatableLcd']]],
   ['printpretty_2',['printPretty',['../classAnimatableLcd.html#ad5509573d45cc84cda93b125fdca749e',1,'AnimatableLcd']]]
 ];
